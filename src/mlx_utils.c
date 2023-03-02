3aa2ade49ce7226eb33c57f9a7df85fafe0e3f8c
Sergio Garcia <sergio@atratopago.com>
2023-03-02 08:18:15 +0100

    Refactor norminette

--- src/mlx_utils.c
+++ src/mlx_utils.c
@@ -6,7 +6,7 @@
 /*   By: segarcia <segarcia@student.42wolfsburg.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 08:01:18 by segarcia          #+#    #+#             */
-/*   Updated: 2023/03/02 08:12:10 by segarcia         ###   ########.fr       */
+/*   Updated: 2023/03/02 08:17:50 by segarcia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,14 @@ int	init_mlx(t_cube3d *data)
 {
 	data->map_size_x = data->map_data.width;
 	data->map_size_y = data->map_data.height;
-	if (!(data->mlx = mlx_init(data->s_width, data->s_height, "CUB3D", 1)))
+	data->mlx = mlx_init(data->s_width, data->s_height, "CUB3D", 1);
+	if (!data->mlx)
 	{
 		write(STDERR_FILENO, MLX_ERROR, 32);
 		return (EXIT_FAILURE);
 	}
-    data->img = mlx_new_image(data->mlx, data->s_width, data->s_height);
-    if (!data->img)
+	data->img = mlx_new_image(data->mlx, data->s_width, data->s_height);
+	if (!data->img)
 	{
 		write(STDERR_FILENO, MLX_IMG_ERROR, 34);
 		return (EXIT_FAILURE);
@@ -30,23 +31,23 @@ int	init_mlx(t_cube3d *data)
 	return (EXIT_SUCCESS);
 }
 
-int init_textures(t_cube3d *data)
+int	init_textures(t_cube3d *data)
 {
 	if (data->we)
 		data->texture_we = mlx_load_png(data->we);
 	if (data->ea)
-   		data->texture_ea = mlx_load_png(data->ea);
- 	if (data->so)
+		data->texture_ea = mlx_load_png(data->ea);
+	if (data->so)
 		data->texture_so = mlx_load_png(data->so);
-   	if (data->no)
+	if (data->no)
 		data->texture_no = mlx_load_png(data->no);
 	if (!data->texture_we
 		|| !data->texture_ea
 		|| !data->texture_so
 		|| !data->texture_no)
-		{
-			write(STDERR_FILENO, MLX_TEXTURE, 32);
-			return (EXIT_FAILURE);
-		}
+	{
+		write(STDERR_FILENO, MLX_TEXTURE, 32);
+		return (EXIT_FAILURE);
+	}
 	return (EXIT_SUCCESS);
 }
