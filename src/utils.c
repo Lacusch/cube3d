7433480027b80ff90525298d8a4be487d1f653ad
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2023-03-10 19:47:30 +0100

    Hotfix 2

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: slaszlo- <slaszlo-@student.42heibronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 02:35:54 by segarcia          #+#    #+#             */
-/*   Updated: 2023/03/10 19:46:52 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2023/03/10 19:47:22 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,8 +84,6 @@ t_map	get_space(char	**matrix)
 bool	check_position(t_player	*player, t_map *map)
 {
 	(void)map;
-	printf("map x is %i map y is %i\n", player->x, player->x);
-
 	if (player->x == 0 || player->y == 0 || \
 		player->x + 1 == map->width || player->y + 1 == map->height)
 		return (true);
