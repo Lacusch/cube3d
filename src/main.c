f12daec5df31c43c5932971961e319aeab1a8f64
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2023-03-10 15:39:19 +0100

    Changed main to test for leaks

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: slaszlo- <slaszlo-@student.42heibronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 08:13:11 by segarcia          #+#    #+#             */
-/*   Updated: 2023/03/10 13:23:50 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2023/03/10 15:38:33 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,27 @@ int32_t	main(int ac, char **av)
 
 	init_data(&data);
 	if (check_arg(ac, av))
+	{
+		system("leaks cub3D");
 		return (EXIT_FAILURE);
+	}
 	input_data(&data, av[1]);
 	if (data.input_error)
-		return (data_free(&data, EXIT_FAILURE));
+	{
+		system("leaks cub3D");
+		data_free(&data, EXIT_FAILURE);
+		return (1);
+	}
 	if (init_textures(&data) == EXIT_FAILURE)
+	{
+		system("leaks cub3D");
 		return (EXIT_FAILURE);
+	}
 	if (init_mlx(&data) == EXIT_FAILURE)
+	{
+		system("leaks cub3D");
 		return (EXIT_FAILURE);
+	}
 	map_cube_size(&data);
 	init_position(&data);
 	draw_3d(&data);
@@ -33,5 +46,6 @@ int32_t	main(int ac, char **av)
 	mlx_loop(data.mlx);
 	mlx_terminate(data.mlx);
 	data_free(&data, EXIT_SUCCESS);
+	system("leaks cub3D");
 	return (EXIT_SUCCESS);
 }
