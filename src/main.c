cb653c3fad6c7086f4aa10713aa5db5eaf83811b
Laszlo M. Szabo <slaszlo-@student.42heilbronn.de>
2023-03-10 19:39:49 +0100

    removed debug stuff

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: slaszlo- <slaszlo-@student.42heibronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 08:13:11 by segarcia          #+#    #+#             */
-/*   Updated: 2023/03/10 16:00:40 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2023/03/10 19:38:10 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,33 +18,19 @@ int32_t	main(int ac, char **av)
 
 	init_data(&data);
 	if (check_arg(ac, av))
-	{
-		system("leaks cub3D");
 		return (EXIT_FAILURE);
-	}
 	input_data(&data, av[1]);
 	if (data.input_error)
-	{
-		data_free(&data, EXIT_FAILURE);
-		system("leaks cub3D");
-		return (1);
-	}
+		return (data_free(&data, EXIT_FAILURE));
 	if (init_textures(&data) == EXIT_FAILURE)
-	{
-		system("leaks cub3D");
 		return (EXIT_FAILURE);
-	}
 	if (init_mlx(&data) == EXIT_FAILURE)
-	{
-		system("leaks cub3D");
 		return (EXIT_FAILURE);
-	}
 	map_cube_size(&data);
 	init_position(&data);
 	draw_3d(&data);
 	mlx_loop_hook(data.mlx, &hook, &data);
 	mlx_loop(data.mlx);
 	mlx_terminate(data.mlx);
-	system("leaks cub3D");
 	return (EXIT_SUCCESS);
 }
--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: slaszlo- <slaszlo-@student.42heibronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/02 02:35:54 by segarcia          #+#    #+#             */
-/*   Updated: 2023/03/10 19:28:34 by slaszlo-         ###   ########.fr       */
+/*   Updated: 2023/03/10 19:38:55 by slaszlo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,7 +84,8 @@ t_map	get_space(char	**matrix)
 bool	check_position(t_player	*player, t_map *map)
 {
 	(void)map;
-	if (player->x + 1 == 0 || player->y + 1 == 0  || player->x + 1 == map->width || player->y + 1 == map->height)
+	if (player->x + 1 == 0 || player->y + 1 == 0 || \
+		player->x + 1 == map->width || player->y + 1 == map->height)
 		return (true);
 	return (false);
 }
